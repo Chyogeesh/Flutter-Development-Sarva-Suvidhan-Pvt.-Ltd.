1c0f7ec2629d21a845db50e2f867a0c21e303036
Ch Yogeesh Sai <108213999+Chyogeesh@users.noreply.github.com>
2025-07-07 10:27:29 +0530

    Create backendconfig.js

---
+++ backendconfig.js
@@ -0,0 +1,12 @@
+{
+  "endpoint": "https://api.example.com/login",
+  "method": "POST",
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "body": {
+    "email": "",
+    "password": "",
+    "biometricToken": ""
+  }
+}
